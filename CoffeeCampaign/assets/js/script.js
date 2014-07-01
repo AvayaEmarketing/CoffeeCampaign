0383af1bce250bceb3c3374c7a1113cd458fd4a6
gerardo cardozo <bremen07@hotmail.com>
2014-07-01 15:19:27 -0500

    Pagína Recursos
    Pagína de recursos + assets

--- CoffeeCampaign/assets/js/script.js
+++ CoffeeCampaign/assets/js/script.js
@@ -6,4 +6,14 @@ $(document).ready(function(){
 	$('.example').mouseleave(function(){
 		$('.card').removeClass('cardvisible');
 	});
+
+	$('.firstcamp').mouseenter(function(){
+		$(this).addClass('campdown');
+	});
+
+	$('.firstcamp').mouseleave(function(){
+		$(this).removeClass('campdown');
+	});
+
+	
 });
